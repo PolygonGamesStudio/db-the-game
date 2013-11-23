f3cd370774dfc6383c7912069c9e0cbf030345c4
sawa2012 <sawa2012@gmail.com>
2013-11-23 14:45:59 +0400

    random word (def)

--- init_db.py
+++ init_db.py
@@ -1,9 +1,11 @@
 #unicoding = utf-8
 
 #from local.py
+from random import choice
 from local import user, password, database
 import requests
 import pymysql
+
 
 USER_AMOUNT = 1000
 CHARACTER_AMOUNT = 1000
@@ -13,18 +15,25 @@ ITEM_AMOUNT = 1000
 CHARACTERISTICS_AMOUNT = 1000
 ABILITY_AMOUNT = 1000
 CLASS_AMOUNT = 1000
+WORD_SITE = "http://www.freebsd.org/cgi/cvsweb.cgi/src/share/dict/web2?rev=1.12;content-type=text%2Fplain"
+WORDS = []
 
-word_site = "http://www.freebsd.org/cgi/cvsweb.cgi/src/share/dict/web2?rev=1.12;content-type=text%2Fplain"
 
-response = requests.get(word_site)
-WORDS = response.content.splitlines()
-print(WORDS)
+def get_word():
+    global WORDS
+    if WORDS:
+        return str(choice(WORDS))
+    else:
+        response = requests.get(WORD_SITE)
+        WORDS += response.content.splitlines()
+        return str(choice(WORDS))
+
 
 connect = pymysql.connect(host='127.0.0.1', port=3306, user=user, passwd=password, db=database)
 cursor = connect.cursor()
 cursor.execute("SHOW TABLES")
 print(cursor.description)
 for row in cursor:
-   print(row)
+    print(row)
 cursor.close()
 connect.close()

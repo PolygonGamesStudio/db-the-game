8e91da75f67e7067bdb8cab1ec6fff2fe32df83b
maratkanov <nemesis1fear0>
2013-11-23 16:16:40 +0400

    table columns added

--- init_db.py
+++ init_db.py
@@ -21,6 +21,16 @@ CLASS_AMOUNT = 1000
 WORD_SITE = "http://www.freebsd.org/cgi/cvsweb.cgi/src/share/dict/web2?rev=1.12;content-type=text%2Fplain"
 WORDS = []
 
+User_columns = ('User_id', 'Firstname', 'Lastname', 'Login', 'Password', 'Registration_date', 'Last_login_date', \
+                'Birthday_date', 'Email', 'is_admin', 'is_active')
+Character_columns = ('Character_id', 'Name', 'Level', 'User_User_id', 'Characteristics_Characteristics_id', \
+                     'Class_Class_id')
+Games_columns = ('Match_Match_id', 'Character_Character_id')
+Match_columns = ('Match_id', 'Title', 'Date_begin', 'Date_end', 'Winner_id', 'Type')
+Class_columns = ('Class_id', 'Type')
+Ability_columns = ('Ability_id', 'Class_Class_id', 'Characteristics_Characteristics_id', 'Title', 'Description')
+
+
 
 def get_word():
     global WORDS
@@ -50,8 +60,6 @@ def fill_insert_sql(column_dict, table ):
         return True
     except ProgrammingError:
         return False
-
-
 
 if __name__ == '__main__':
     fill_insert_sql({"first_name": "alexei", "last_name": 'maratlanov', 'is_active': '1'}, 'User')

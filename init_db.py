1799bde08844f86b18c10dd58e61529780040458
sawa2012 <sawa2012@gmail.com>
2013-11-23 13:02:36 +0400

    simple example

--- init_db.py
+++ init_db.py
@@ -1,6 +1,15 @@
 #unicoding = utf-8
+#!/usr/bin/env python
 
 #from local.py
-from local import user, password
+from local import user, password, database
+import pymysql
 
-print(user, password)
+connect = pymysql.connect(host='127.0.0.1', port=3306, user=user, passwd=password, db=database)
+cursor = connect.cursor()
+cursor.execute("SHOW TABLES")
+print(cursor.description)
+for row in cursor:
+   print(row)
+cursor.close()
+connect.close()
